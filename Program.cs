44e6f8b9d0d86dfd32cacd38f9b9f1ee649ad6fd
вадим <funpalka1@gmail.com>
2023-05-16 22:39:16 +0400

    Init commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,130 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Console.Write("Введите количество строк массива: ");
+// int rows = int.Parse(Console.ReadLine());
+
+// Console.Write("Введите количество столбцов массива: ");
+// int columns = int.Parse(Console.ReadLine());
+
+// int[,] array = new int[rows, columns];
+
+// for (int i = 0; i < rows; i++)
+// {
+//     for (int j = 0; j < columns; j++)
+//     {
+//         array[i, j] = new Random().Next(-10, 10 + 1);
+//     }
+// }
+
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         Console.Write($"{array[i, j]} ");
+//     }
+//     Console.WriteLine();
+// }
+// Задачи семинар 7 C#
+
+// Задача 1: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле:
+// Aₘₙ = m+n. Выведите полученный массив на экран.
+
+// m = 3, n = 4.
+
+// 0 1 2 3
+
+// 1 2 3 4
+
+// 2 3 4 5
+
+
+// Console.Write("Введите количество строк массива: ");
+// int rows = int.Parse(Console.ReadLine());
+
+// Console.Write("Введите количество столбцов массива: ");
+// int columns = int.Parse(Console.ReadLine());
+
+// int [,] array = new int [rows , columns];
+
+// for (int i = 0; i < rows; i++)
+// {
+//     for (int j = 0; j < columns; j++)
+//     {
+//         array[i , j] = i + j ;
+//     }
+// }
+
+// for (int i = 0; i < rows; i++)
+// {
+//     for (int j = 0; j < columns; j++)
+//     {
+//         Console.Write($"{array[i,j]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+
+
+// Задача 2: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные,
+// и замените эти элементы на их квадраты.
+
+// Например, изначально массив выглядел вот так:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 3 2 9
+
+// Новый массив будет выглядеть вот так:
+
+// 1 4 49 2
+
+// 5 9 2 9
+
+// 64 3 4 9
+
+// Console.Write("Введите количество строк массива: ");
+// int rows = int.Parse(Console.ReadLine());
+
+// Console.Write("Введите количество столбцов массива: ");
+// int columns = int.Parse(Console.ReadLine());
+
+// int[,] array = new int[rows, columns];
+
+// for (int i = 0; i < rows; i++)
+// {
+//     for (int j = 0; j < columns; j++)
+//     {
+//         array[i, j] = new Random().Next(-10, 10 + 1);
+//     }
+// }
+
+// for (int i = 0; i < rows; i++)
+// {
+//     for (int j = 0; j < columns; j++)
+//     {
+//         Console.Write($"{array[i, j]} ");
+//     }
+//     Console.WriteLine();
+// }
+// Console.WriteLine();
+
+
+// for (int i = 0; i < rows; i++)
+// {
+//     for (int j = 0; j < columns; j++)
+//     {
+//         if (i % 2 == 0 && j % 2 == 0)
+//         {
+//             array[i, j] = array[i, j] * array[i, j];
+//         }
+//     }
+// }
+
+// for (int i = 0; i < rows; i++)
+// {
+//     for (int j = 0; j < columns; j++)
+//     {
+//         Console.Write($"{array[i, j]} ");
+//     }
+//     Console.WriteLine();
+// }
